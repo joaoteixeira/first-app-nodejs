88725d2c56bcfd3d8f77e5dc7e0ad66664b72c0f
João Teixeira <joao.teixeira@ifro.edu.br>
2022-07-27 22:00:37 -0400

    Código inicial

---
+++ main.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index', {
+    text: 'Iniciando com NodeJs',
+    data: '27/07/22'
+  });
+})
+
+app.get('/sobre', function(request, response) {
+  response.render('pages/sobre');
+})
+
+app.listen(8000, function() {
+  console.log('Server is Run - port:8000');
+});
